2aeb5a5dae9a2803f839a38ebb7a4e0a077ae56d
ShinghoiXu <41323710+ShinghoiXu@users.noreply.github.com>
2023-12-16 22:08:18 +0000

    Easing Functions Added

--- data/Grainy.glsl
+++ data/Grainy.glsl
@@ -12,6 +12,22 @@ uniform int grainyR;
 varying vec4 vertColor;
 varying vec4 vertTexCoord;
 
+float easeInExpo(float x) {
+    if (x == 0.0) {
+        return 0.0;
+    } else {
+        return pow(2.0, 10.0 * x - 10.0);
+    }
+}
+
+float easeInOutSine(float x) {
+    return -(cos(3.141592653589793 * x) - 1.0) / 2.0;
+}
+
+float easeOutCirc(float x) {
+    return sqrt(1.0 - pow(x - 1.0, 2.0));
+}
+
 void main(void) {
   
   float mixFactor = 0.5;
